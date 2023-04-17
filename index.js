ffdeba85e04607ff22a46b7c65d0bddb40d65bd3
Becca <112990922+rebecacarvalhoaraujo@users.noreply.github.com>
2023-04-17 12:14:06 -0300

    atividade backend

---
+++ index.js
@@ -0,0 +1,18 @@
+// Importações principais e variáveis de ambiente
+require("dotenv").config();
+const express = require("express");
+
+// Configuração do App
+const app = express();
+app.use(express.json()); // Possibilitar transitar dados usando JSON
+
+// Configuração do Banco de Dados
+const { connection, authenticate } = require("./database/database");
+authenticate(connection); // efetivar a conexão
+
+// Definição de rotas
+
+// Escuta de eventos (listen)
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
