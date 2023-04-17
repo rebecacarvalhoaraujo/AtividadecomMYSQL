432f24820208d4fb1be0523ba0986ef7b593ebfc
Becca <112990922+rebecacarvalhoaraujo@users.noreply.github.com>
2023-04-17 20:11:33 -0300

    atuaçizações da aula de 1704

---
+++ database/cliente.js
@@ -0,0 +1,37 @@
+// Modelo para gerar a tabela de clientes no MySQL
+// Mapeamento: cada propriedade vira uma coluna da tabela
+
+// DataTypes = serve para definir qual o tipo da coluna
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+
+const Cliente = connection.define("cliente", {
+  // Configurar a coluna 'nome'
+  nome: {
+    // nome VARCHAR NOT NULL
+    type: DataTypes.STRING(130),
+    allowNull: false, // NOT NULL
+  },
+  email: {
+    // email VARCHAR UNIQUE NOT NULL
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  telefone: {
+    // telefone VARCHAR NOT NULL
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+// Associação 1:1 (One-to-One)
+const Endereco = require("./endereco");
+
+// Cliente tem um Endereço
+// Endereço ganha uma chave estrangeira (nome do model + Id)
+// Chave estrangeira = clienteId
+Cliente.hasOne(Endereco);
+Endereco.belongsTo(Cliente); // Endereço pertence a um Cliente
+
+module.exports = Cliente;
